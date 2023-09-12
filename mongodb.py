ef97f2c41ac79ccb4147ba430ba127fc9125f9de
darroyo606 <dcarroyop@gmail.com>
2023-09-11 19:19:57 -0500

    segundo commit

---
+++ mongodb.py
@@ -0,0 +1,20 @@
+
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
+user=os.getenv('MONGO_USER')
+password=os.getenv('MONGO_PASSWORD')
+
+
+uri = f"mongodb+srv://{user}:{password}@cluster0.ij08aaa.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
